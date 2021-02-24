2c67cd8a9e0bfafac57cd04dca06a246f3dc69d2
Torrax <Carson.morris2@gmail.com>
2021-02-24 17:22:47 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -9,22 +9,64 @@ local event = require("event")
 local application = GUI.application()
 
 -- Create and add template object to application
-local invManager = application:addChild(GUI.object(5, 3, 35, 10))
+local taskbar = application:addChild(GUI.object(0, 0, 165, 5))
+
+local invManager = application:addChild(GUI.object(17, 11, 35, 12))
+local iFace = application:addChild(GUI.object(62, 11, 35, 12))
+local drone = application:addChild(GUI.object(107, 11, 35, 12))
+local nuclear = application:addChild(GUI.object(17, 28, 35, 12))
+local security = application:addChild(GUI.object(62, 28, 35, 12))
+local redstone = application:addChild(GUI.object(107, 28, 35, 12))
 local exit = application:addChild(GUI.object(145, 45, 15, 5))
 
 --Clear Screen and Set Background Colour
 buffer.clear(0x999999)
 
+-- Draw Taskbar
+taskbar.draw = function(object)
+    buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
+end
+
 -- Draw Inventory Button and Text
 invManager.draw = function(object)
     buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
-    buffer.drawText(object.x + 14, object.y + 5, 0x999999, "Inventory")
+    buffer.drawText(object.x + 14, object.y + 6, 0x999999, "Inventory")
 end
 
--- Draw Exit Button and Text
+-- Draw iFace Button and Text
+iFace.draw = function(object)
+    buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
+    buffer.drawText(object.x + 16, object.y + 6, 0x999999, "iFace")
+end
+
+-- Draw Drone Control Button and Text
+drone.draw = function(object)
+    buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
+    buffer.drawText(object.x + 12, object.y + 6, 0x999999, "Drone Control")
+end
+
+-- Draw Nuclear Control Button and Text
+nuclear.draw = function(object)
+    buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
+    buffer.drawText(object.x + 10, object.y + 6, 0x999999, "Nuclear Control")
+end
+
+-- Draw Security Control Button and Text
+security.draw = function(object)
+    buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
+    buffer.drawText(object.x + 10, object.y + 6, 0x999999, "Security Control")
+end
+
+-- Draw Redstone Control Button and Text
+redstone.draw = function(object)
+    buffer.drawRectangle(object.x, object.y, object.width, object.height, 0x666666, 0x0, " ")
+    buffer.drawText(object.x + 10, object.y + 6, 0x999999, "Redstone Control")
+end
+
+-- Draw System Button and Text
 exit.draw = function(object)
     buffer.drawRectangle(object.x, object.y, object.width, object.height, 0xE10000, 0x0, " ")
-    buffer.drawText(object.x + 6, object.y + 2, 0x999999, "Exit")
+    buffer.drawText(object.x + 5, object.y + 2, 0x999999, "System")
 end
 
 --------------------------------------------------------------------------------
