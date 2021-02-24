ea1e4727c8fbb4416a9a32df1bef1a9f72eeb8e3
Torrax <Carson.morris2@gmail.com>
2021-02-24 11:09:18 -0500

    Delete ae2-manager-master directory

--- ae2-manager-master/dev/serve.bat
+++
@@ -1,2 +0,0 @@
-REM You may need to remove localhost from the OpenComputers blacklist
-python -m http.server --directory ../src 40000
--- ae2-manager-master/dev/serve.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# You may need to remove localhost from the OpenComputers blacklist
-exec python3 -m http.server --directory ../src 40000
