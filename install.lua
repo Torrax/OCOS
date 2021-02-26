3da6ee5bfe1b1dbacb5286d52d06851dfd856b6f
Torrax <Carson.morris2@gmail.com>
2021-02-25 20:02:02 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,9 +2,9 @@ local REPOSITOTY = "https://raw.githubusercontent.com/Torrax/OCOS/main"
 
 local shell = require("shell")
 -- Libraries
-shell.execute("wget -fq " .. REPOSITOTY .. "/libs/Image.lua /lib/Image.lua")
+shell.execute("wget -fq " .. REPOSITOTY .. "/libs/image.lua /lib/image.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/libs/doubleBuffering.lua /lib/doubleBuffering.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/libs/GUI.lua /lib/GUI.lua")
+shell.execute("wget -fq " .. REPOSITOTY .. "/libs/gui.lua /lib/gui.lua")
 
 -- Scripts
 shell.execute("wget -fq " .. REPOSITOTY .. "/ui.lua /home/ui.lua")
