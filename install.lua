05ffd47e3f9c773775e37ec0a7d9c6589e02dc39
Torrax <Carson.morris2@gmail.com>
2021-02-26 03:39:16 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -10,7 +10,8 @@ shell.execute("wget -fq " .. REPOSITOTY .. "/lib/gui.lua /lib/gui.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/ui.lua /home/ui.lua")
 --shell.execute("wget -fq " .. REPOSITOTY .. "/ui.lua /autorun.lua")
 
-shell.execute("wget -fq " .. REPOSITOTY .. "/apps/invManager/ae2-manager.lua /home/apps/stocker.lua")
+shell.execute("mkdir /home/apps")
+shell.execute("wget -fq " .. REPOSITOTY .. "/apps/stocker/ae2-manager.lua /home/apps/stocker.lua")
 
 -- Images
 shell.execute("mkdir /home/images")
