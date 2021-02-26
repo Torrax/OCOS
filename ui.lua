c7306959b0d4e1227a296266dd315faa430825e8
Torrax <Carson.morris2@gmail.com>
2021-02-26 03:10:53 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,19 +1,33 @@
 -- Import libraries
-local GUI = require("gui")
 local buffer = require("doubleBuffering")
 local shell = require("shell")
 local event = require("event")
 local image = require("image")
 --------------------------------------------------------------------------------
--- Create new application
-local application = GUI.application()
-
 local function drawStatic()
-    buffer.setResolution(160, 50)     -- Set Resolution 160 x 50
+    -- Set Resolution 160 x 50
+    buffer.setResolution(160, 50)
     
-    buffer.drawImage(1 , 1, image.load("/home/images/torUI.pic"))   -- UI Background
-    application:addChild(GUI.panel(9, 2, 14, 3, 0x999999))          -- System Time Box ******************************* Switch to buffer
-    application:addChild(GUI.panel(142, 2, 11, 3, 0x000000))        -- Empty Battery  ************************************************
+    -- Draw Background
+    buffer.drawImage(1 , 1, image.load("/home/images/torUI.pic"))
+    
+    -- Clock
+    buffer.drawSemiPixelRectangle( 9, 2, 17, 8, 0x000000)
+    buffer.drawSemiPixelRectangle( 10, 3, 15, 6, 0x999999)
+    buffer.drawText(13, 3, 0x000000, (os.date("%H:%M:%S", os.time())));
+    
+    -- Battery
+    buffer.drawSemiPixelRectangle( 141, 2, 12, 8, 0x000000)
+    buffer.drawSemiPixelRectangle( 142, 3, 11, 6, 0x999999)
+    
+    buffer.drawSemiPixelRectangle( 142, 3, 11, 6, 0x22DA00)
+    buffer.drawText(13, 3, 0x000000, "99 %");
+    
+    buffer.drawSemiPixelRectangle( 152, 2, 1, 3, 0x000000)
+    buffer.drawSemiPixelRectangle( 152, 7, 1, 3, 0x999999)
+    buffer.drawSemiPixelRectangle( 153, 4, 1, 4, 0x000000)
+    buffer.drawSemiPixelRectangle( 153, 2, 1, 2, 0x666666)
+    buffer.drawSemiPixelRectangle( 153, 8, 1, 2, 0x666666)
     
     -- Draw Application Buttons ************************ Switch to Images
     buffer.drawImage(17 , 11, image.load("/home/images/app_Stocker.pic"))   -- Stocker Logo Button
@@ -22,18 +36,13 @@ local function drawStatic()
     buffer.drawImage(17 , 28, image.load("/home/images/app_nuclearController.pic"))   -- Nuclear Control Logo Button
     buffer.drawImage(62 , 28, image.load("/home/images/app_droneControl.pic"))   -- Security Panel Logo Button
     buffer.drawImage(107 , 28, image.load("/home/images/app_remoteRedstone.pic"))   -- Remote Redstone Logo Button
-    application:addChild(GUI.panel(145, 45, 15, 5, 0xE10000))
+
+    buffer.drawSemiPixelRectangle( 144, 89, 16, 10, 0x000000)
+    buffer.drawSemiPixelRectangle( 145, 90, 14, 8, 0x474747)
     application:addChild(GUI.label(145, 45, 15, 5, 0x999999, "System")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
 end
 
 local function drawDynamic()
-    -- Game Clock
-    buffer.drawText(12, 3, 0x000000, (os.date("%H:%M:%S", os.time())));
-    -- Battery
-    application:addChild(GUI.panel(142, 2, 11, 3, 0x22DA00))  --Charged
-    application:addChild(GUI.panel(152, 2, 1, 1, 0x666666))   --Blocker 1
-    application:addChild(GUI.panel(152, 4, 1, 1, 0x666666))   --Blocker 2
-    application:addChild(GUI.label(142, 2, 10, 3, 0x000000, "99 %")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
 end
 
 buffer.flush()
@@ -57,7 +66,7 @@ while true do
     
     -- Inventory Button
     if x >= 17 and x <= 51 and y >= 11 and y <= 22 then
-        shell.execute("/usr/apps/invManager.lua")
+        shell.execute("/usr/apps/stocker.lua")
         os.exit()
     end
     
@@ -69,25 +78,25 @@ while true do
     
     -- Drone Control Button
     if x >= 107 and x <= 141 and y >= 11 and y <= 22 then
-        --shell.execute("/usr/apps/droneCtrl.lua")
+        --shell.execute("/usr/apps/droneControl.lua")
         os.exit()
     end
     
     -- Nuclear Control Button
     if x >= 17 and x <= 51 and y >= 28 and y <= 39 then
-        --shell.execute("/usr/apps/nuclearCtrl.lua")
+        --shell.execute("/usr/apps/nuclearController.lua")
         os.exit()
     end
     
     -- Security Control Button
     if x >= 62 and x <= 96 and y >= 28 and y <= 39 then
-        --shell.execute("/usr/apps/securityCtrl.lua")
+        --shell.execute("/usr/apps/securityPanel.lua")
         os.exit()
     end
     
     -- Redstone Control Button
     if x >= 107 and x <= 141 and y >= 28 and y <= 39 then
-        --shell.execute("/usr/apps/redstoneCtrl.lua")
+        --shell.execute("/usr/apps/remoteRedstone.lua")
         os.exit()
     end
     
