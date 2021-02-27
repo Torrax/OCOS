f5689480013e476c9872813b05413cdac374328c
Torrax <Carson.morris2@gmail.com>
2021-02-27 04:29:07 -0500

    Update remoteRedstone.lua

--- apps/remoteRedstone.lua
+++ apps/remoteRedstone.lua
@@ -1,53 +1,186 @@
--- REDBoardControl ver 1.2 ENG by Laine_prikol
-forms=require("forms")
-local term = require("term")
-local sides = require("sides")
+-- Libraries
 local computer = require("computer")
+local sides = require("sides")
+local shell = require("shell")
 local component = require("component")
-local rs = component.redstone
-rs.setOutput(sides.south, 0)
- 
-Form1=forms.addForm()
-Form1.border=2
-term.setCursor(2,2)
--- authors
-Label1=Form1:addLabel(30,25,"Program author: Laine_prikol Author API: Zer0Galaxy")
-Label1=Form1:addLabel(2,1,"REDBOARD Control ver 1.2")
--- Panel 1
-Label1=Form1:addLabel(2,2,"Contol front side:")
-Btn1=Form1:addButton(2,3,"ON",function() rs.setOutput(sides.south, 15) computer.beep(600,0.5)  end) 
-Btn1.color=0xfffff                       
-Btn2=Form1:addButton(2,4,"OFF",function() rs.setOutput(sides.south, 0) computer.beep(100,0.8) end) 
-Btn2.color=0xff0000
-Btn3=Form1:addButton(2,5,"Cycle",function() while true do rs.setOutput(sides.south, 0) rs.setOutput(sides.south, 15) os.sleep(0.5) end   end)
-Btn3.color=0x505050
--- Panel 2
-Label1=Form1:addLabel(2,6,"Contol left side:")
-Btn4=Form1:addButton(2,7,"ON",function() rs.setOutput(sides.west, 15) computer.beep(600,0.5)  end) 
-Btn4.color=0xfffff
-Btn5=Form1:addButton(2,8,"OFF",function() rs.setOutput(sides.west, 0) computer.beep(100,0.8) end) 
-Btn5.color=0xff0000
-Btn6=Form1:addButton(2,9,"Cycle",function() while true do rs.setOutput(sides.west, 0) rs.setOutput(sides.west, 15) end os.sleep(0.5)  end)
-Btn6.color=0x505050
--- Panel 3
-Label1=Form1:addLabel(2,10,"Control back side")
-Btn7=Form1:addButton(2,11,"ON ",function() rs.setOutput(sides.north, 15) computer.beep(600,0.5)  end) 
-Btn7.color=0xfffff
-Btn8=Form1:addButton(2,12,"OFF",function() rs.setOutput(sides.north, 0) computer.beep(100,0.8) end) 
-Btn8.color=0xff0000
-Btn9=Form1:addButton(2,13,"Cycle",function() while true do rs.setOutput(sides.north, 0) rs.setOutput(sides.north, 15) os.sleep(0.5) end   end)
-Btn9.color=0x505050
--- Panel 4
-Label1=Form1:addLabel(40,2,"Contol right side")
-Btn10=Form1:addButton(40,3,"ON ",function() rs.setOutput(sides.east, 15) computer.beep(600,0.5)  end) 
-Btn10.color=0xfffff
-Btn11=Form1:addButton(40,4,"OFF",function() rs.setOutput(sides.east, 0) computer.beep(100,0.8) end) 
-Btn11.color=0xff0000
-Btn12=Form1:addButton(40,5,"Cycle",function() while true do rs.setOutput(sides.east, 0) rs.setOutput(sides.east, 15) end os.sleep(0.5)  end)
-Btn12.color=0x505050
-Label1=Form1:addLabel(40,9,"Miscellaneous:")
-Btn1=Form1:addButton(40,10,"ALARM",function() while true do computer.beep(775,0.5) end os.sleep(0.5)  end) 
-Btn1.color=0xfffff
-Btn1=Form1:addButton(40,11,"EXIT",function() forms.stop() term.clear() end)
-Btn1.color=0x505050
-forms.run(Form1) -- Load Form1
+local GUI = require("gui")
+
+local nPower = 50
+local sPower = 50
+local ePower = 50
+local wPower = 50
+local tPower = 50
+local bPower = 50
+
+-- Create new workspace
+local workspace = GUI.application()
+
+--Red Outter Panel
+workspace:addChild(GUI.panel(0, 0, 161, 101, 0x880000))
+--Grey Inlay
+workspace:addChild(GUI.panel(5, 8, 151, 40, 0x262626))
+--Label Boxes
+workspace:addChild(GUI.panel(60, 12, 15, 3, 0xE1E1E1))
+workspace:addChild(GUI.label(60, 12, 15, 3, 0x4B4B4B, "North")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
+workspace:addChild(GUI.panel(110, 12, 15, 3, 0xE1E1E1))
+workspace:addChild(GUI.label(110, 12, 15, 3, 0x4B4B4B, "South")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
+workspace:addChild(GUI.panel(60, 22, 15, 3, 0xE1E1E1))
+workspace:addChild(GUI.label(60, 22, 15, 3, 0x4B4B4B, "East")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
+workspace:addChild(GUI.panel(110, 22, 15, 3, 0xE1E1E1))
+workspace:addChild(GUI.label(110, 22, 15, 3, 0x4B4B4B, "West")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
+workspace:addChild(GUI.panel(60, 32, 15, 3, 0xE1E1E1))
+workspace:addChild(GUI.label(60, 32, 15, 3, 0x4B4B4B, "Top")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
+workspace:addChild(GUI.panel(110, 32, 15, 3, 0xE1E1E1))
+workspace:addChild(GUI.label(110, 32, 15, 3, 0x4B4B4B, "Bottom")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
+
+-- Quit Button
+local notAnimatedButton = workspace:addChild(GUI.button(132, 2, 25, 3, 0xAAAAAA, 0x4B4B4B, 0xE1E1E1, 0x4B4B4B, "Exit"))
+notAnimatedButton.animated = false
+notAnimatedButton.onTouch = function()
+	shell.execute("/home/ui.lua")
+        os.close()
+end
+
+-- Controller List
+local verticalList = workspace:addChild(GUI.list(10, 13, 25, 30, 3, 0, 0xE1E1E1, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false))
+verticalList:addItem("Controller 1")
+verticalList:addItem("Controller 2").onTouch = function()
+	GUI.alert("Controller Selected " .. verticalList.selectedItem) --FUNCTION
+end
+verticalList:addItem("Controller 3")
+verticalList:addItem("Controller 4")
+
+-- Controller Buttons
+workspace:addChild(GUI.roundedButton(50, 15, 15, 3, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ON")).onTouch = function() -- North ON
+	component.redstone.setOutput(sides.north, nPower) computer.beep(600,0.5)
+end
+workspace:addChild(GUI.roundedButton(70, 15, 15, 3, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "OFF")).onTouch = function() -- North OFF
+	component.redstone.setOutput(sides.north, 0) computer.beep(100,0.8)
+end
+local slider = workspace:addChild(GUI.slider(51, 19, 32, 0x66DB80, 0x0, 0xFFFFFF, 0xAAAAAA, 0, 50, 50, true, "Signal Strength: ", ""))  -- North Signal
+slider.roundValues = true
+slider.onValueChanged = function()
+	nPower = slider.value
+end
+
+workspace:addChild(GUI.roundedButton(100, 15, 15, 3, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ON")).onTouch = function() -- South ON
+	component.redstone.setOutput(sides.south, sPower) computer.beep(600,0.5)
+end
+workspace:addChild(GUI.roundedButton(120, 15, 15, 3, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "OFF")).onTouch = function() -- South OFF
+	component.redstone.setOutput(sides.south, 0) computer.beep(100,0.8)
+end
+local slider = workspace:addChild(GUI.slider(101, 19, 32, 0x66DB80, 0x0, 0xFFFFFF, 0xAAAAAA, 0, 50, 50, true, "Signal Strength: ", "")) -- South Signal
+slider.roundValues = true
+slider.onValueChanged = function()
+	sPower = slider.value
+end
+
+workspace:addChild(GUI.roundedButton(50, 25, 15, 3, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ON")).onTouch = function() -- East ON
+	component.redstone.setOutput(sides.east, ePower) computer.beep(600,0.5)
+end
+workspace:addChild(GUI.roundedButton(70, 25, 15, 3, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "OFF")).onTouch = function() -- East OFF
+	component.redstone.setOutput(sides.east, 0) computer.beep(100,0.8)
+end
+local slider = workspace:addChild(GUI.slider(51, 29, 32, 0x66DB80, 0x0, 0xFFFFFF, 0xAAAAAA, 0, 50, 50, true, "Signal Strength: ", "")) -- East Signal
+slider.roundValues = true
+slider.onValueChanged = function()
+	ePower = slider.value
+end
+
+workspace:addChild(GUI.roundedButton(100, 25, 15, 3, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ON")).onTouch = function() -- West ON
+	component.redstone.setOutput(sides.west, wPower) computer.beep(600,0.5)
+end
+workspace:addChild(GUI.roundedButton(120, 25, 15, 3, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "OFF")).onTouch = function() -- West OFF
+	component.redstone.setOutput(sides.west, 0) computer.beep(100,0.8)
+end
+local slider = workspace:addChild(GUI.slider(101, 29, 32, 0x66DB80, 0x0, 0xFFFFFF, 0xAAAAAA, 0, 50, 50, true, "Signal Strength: ", "")) -- West Signal
+slider.roundValues = true
+slider.onValueChanged = function()
+	wPower = slider.value
+end
+
+workspace:addChild(GUI.roundedButton(50, 35, 15, 3, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ON")).onTouch = function() -- Top ON
+	component.redstone.setOutput(sides.top, tPower) computer.beep(600,0.5)
+end
+workspace:addChild(GUI.roundedButton(70, 35, 15, 3, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "OFF")).onTouch = function() -- Top OFF
+	component.redstone.setOutput(sides.top, 0) computer.beep(100,0.8)
+end
+local slider = workspace:addChild(GUI.slider(51, 39, 32, 0x66DB80, 0x0, 0xFFFFFF, 0xAAAAAA, 0, 50, 50, true, "Signal Strength: ", "")) -- Top Signal
+slider.roundValues = true
+slider.onValueChanged = function()
+	tPower = slider.value
+end
+
+workspace:addChild(GUI.roundedButton(100, 35, 15, 3, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ON")).onTouch = function() -- Bottom ON
+	component.redstone.setOutput(sides.bottom, bPower) computer.beep(600,0.5)
+end
+workspace:addChild(GUI.roundedButton(120, 35, 15, 3, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "OFF")).onTouch = function() -- Bottom Off
+	component.redstone.setOutput(sides.bottom, 0) computer.beep(100,0.8)
+end
+local slider = workspace:addChild(GUI.slider(101, 39, 32, 0x66DB80, 0x0, 0xFFFFFF, 0xAAAAAA, 0, 50, 50, true, "Signal Strength: ", "")) -- Bottom Signal
+slider.roundValues = true
+slider.onValueChanged = function()
+	bPower = slider.value
+end
+
+-- All Control
+local notAnimatedButton = workspace:addChild(GUI.button(63, 43, 25, 4, 0x22DA00, 0x4B4B4B, 0x148700, 0x4B4B4B, "ALL ON"))
+notAnimatedButton.animated = false
+notAnimatedButton.onTouch = function()
+	component.redstone.setOutput(sides.north, nPower)
+	component.redstone.setOutput(sides.south, sPower)
+	component.redstone.setOutput(sides.east, ePower)
+	component.redstone.setOutput(sides.west, wPower)
+	component.redstone.setOutput(sides.top, tPower)
+	component.redstone.setOutput(sides.bottom, bPower) computer.beep(750,0.8)
+end
+
+local notAnimatedButton = workspace:addChild(GUI.button(93, 43, 25, 4, 0xE30000, 0x4B4B4B, 0x880000, 0x4B4B4B, "ALL OFF"))
+notAnimatedButton.animated = false
+notAnimatedButton.onTouch = function()
+	component.redstone.setOutput(sides.north, 0)
+	component.redstone.setOutput(sides.south, 0)
+	component.redstone.setOutput(sides.east, 0)
+	component.redstone.setOutput(sides.west, 0)
+	component.redstone.setOutput(sides.top, 0)
+	component.redstone.setOutput(sides.bottom, 0) computer.beep(250,1.2)
+end
+
+--------------------------------------------------------------------------------
+
+-- Get Redstone Devices
+function getRedstoneIO()
+  local countRedstoneIO = 0
+	
+	local RedstoneIO = component.list("redstone")
+	
+	local redstoneID = {}	
+	for address, name in pairs(RedstoneIO) do
+		countRedstoneIO =  countRedstoneIO + 1
+		if countRedstoneIO > 1 then
+			redstoneID[address] = "Redstone Controller" .. " " .. countRedstoneIO
+		else
+			redstoneID[address] ="Redstone Controller"
+		end	
+	end
+  return cellsID
+end
+
+-- Start Script
+workspace:draw()
+workspace:start()
+
+local controllerID = getRedstoneIO()
+
+while true do
+  local _,_,x,y = event.pull( 1, "touch" )
+  local count = 0 
+  for address, name in pairs(controllerID) do
+    
+	    local cell = component.proxy( address )
+	    count = count + 1
+	    local x1 = count * 3
+	    --progressBar( name, t , cell.getEnergyStored(), cell.getMaxEnergyStored() , 0x00bb00, true, "RF" )
+	end
+  os.sleep(0.25)
+end
